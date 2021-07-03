62616104558ddbceb01a21a79f65b86807682f15
Aman Saxena <saxenaaman628@gmail.com>
2021-07-03 12:15:58 +0530

    learning react

---
+++ src/ToDoForm.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+
+const ToDoForm = ({addTask}) =>{
+  const [userInput,setUserInput] =useState('');
+  const handleChange =(e)=>{
+      setUserInput(e.currentTarget.value);
+  }
+  const handleSubmit =(e)=>{
+      e.preventDefault();
+      addTask(userInput);
+      console.log(userInput);
+      setUserInput('');
+  }
+    return (
+        <form onSubmit={handleSubmit}>
+        <input value={userInput} type='text' onChange={handleChange}
+        placeholder="Enter task..."/> 
+        <button type='submit'>Submit</button>
+      </form>
+  );  
+} 
+export default ToDoForm;
